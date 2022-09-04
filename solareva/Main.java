9c3e79fe3642224abb56bb79dfad505484c6b754
y0lia <111846282+y0lia@users.noreply.github.com>
2022-09-04 05:41:25 +0300

    3
    N школярів ділять K яблук порівну, залишок, що не ділиться, залишається в кошику.
    Скільки яблук дістанеться кожному школяреві?

---
+++ solareva/Main.java
@@ -0,0 +1,27 @@
+package com.solareva;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+
+     public static void main(String[] args) throws IOException {
+    	 BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    	 
+    	 System.out.print("Введіть кіл-ть школярів: ");
+    	 String a= reader.readLine();
+    	 int x = Integer.parseInt(a);
+    	 System.out.print("Введіть кіл-ть яблук : ");
+    	 String b= reader.readLine();
+    	 int y = Integer.parseInt(b);
+
+    	 System.out.print("Кожному буде по ");
+        int z;
+        z = y/x;
+
+        
+        System.out.println(z);
+     }
+  }
